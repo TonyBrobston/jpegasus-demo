87aa804f73c01302d612d2aa2328e13a8d0a7a79
Tony Brobston <tbrobston@hy-vee.com>
2019-01-15 16:14:37 -0600

    Hardcode maxWidth also for now

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,8 @@ const Jpegasus = require('Jpegasus');
 compress = function(file) {
     document.getElementById('originalDetails').innerHTML = `{size: ${file.size}, type: ${file.type}}`;
     Jpegasus.compress(file, {
-        maxHeight: 1000
+        maxHeight: 1000,
+        maxWidth: 1000
     }).then(function (compressedFile) {
         document.getElementById('compressedDetails').innerHTML = `{size: ${compressedFile.size}, type: ${compressedFile.type}}`;
         document.getElementById('compressedImg').src = URL.createObjectURL(compressedFile);
