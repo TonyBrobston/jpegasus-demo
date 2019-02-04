ad397a72306061daa637d58f7a1339e143f955b7
Tony Brobston <tbrobston@hy-vee.com>
2019-02-04 15:01:54 -0600

    Adding quality selector

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,34 @@
 const Jpegasus = require('Jpegasus');
 
 compress = function(file) {
-    document.getElementById('originalDetails').innerHTML = `{size: ${file.size}, type: ${file.type}}`;
-    Jpegasus.compress(file, {
-        maxHeight: 1000,
-        maxWidth: 1000
-    }).then(function (compressedFile) {
-        document.getElementById('compressedDetails').innerHTML = `{size: ${compressedFile.size}, type: ${compressedFile.type}}`;
-        document.getElementById('compressedImg').src = URL.createObjectURL(compressedFile);
-    });
+    if (file) {
+        document.getElementById('originalDetails').innerHTML = `{size: ${file.size}, type: ${file.type}}`;
+
+        const quality = document.getElementById('qualitySelector').value;
+
+        Jpegasus.compress(file, {
+            maxHeight: 1000,
+            maxWidth: 1000,
+            quality: parseFloat(quality)
+        }).then(function (compressedFile) {
+            document.getElementById('compressedDetails').innerHTML = `{size: ${compressedFile.size}, type: ${compressedFile.type}}`;
+            document.getElementById('compressedImg').src = URL.createObjectURL(compressedFile);
+        });
+    }
+};
+
+window.onload = function() {
+    const minimumQuality = 1;
+    const maxQuality = 100;
+    const qualitySelector = document.getElementById('qualitySelector');
+
+    for (let i = maxQuality; i >= minimumQuality; i--) {
+        const option = document.createElement('option');
+        const quality = parseFloat(i / 100).toFixed(2);
+
+        option.text = quality;
+        option.value = quality;
+
+        qualitySelector.appendChild(option);
+    }
 };
