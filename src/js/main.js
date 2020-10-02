54fef2d8e27631680de1906d987c87e77d83bd8d
Tony Brobston <anthonybrobston@live.com>
2020-10-02 10:58:31 -0500

    Add console.info for file and compressedFile

--- src/js/main.js
+++ src/js/main.js
@@ -13,19 +13,15 @@ compressAndReportResults = async () => {
     const file = document.getElementById('imageInput').files[0];
 
     if (file) {
-        const originalOrientation = await determineOrientation(file);
-        setOriginalDetails(file, originalOrientation);
+        setOriginalDetails(file);
 
         const maxHeight = parseFloat(getValue('maxHeightSelector'));
         const maxWidth = parseFloat(getValue('maxWidthSelector'));
         const scaleImageBy = parseFloat(getValue('scaleImageBy'));
         const quality = parseFloat(getValue('qualitySelector'));
-        const returnOriginalIfCompressedFileIsLarger =
-          'true' === getValue('returnOriginalIfCompressedFileIsLargerSelector');
-        const returnOriginalOnFailure =
-          'true' === getValue('returnOriginalOnFailureSelector');
-        const fixImageOrientation =
-          'true' === getValue('fixImageOrientation');
+        const returnOriginalIfCompressedFileIsLarger = 'true' === getValue('returnOriginalIfCompressedFileIsLargerSelector');
+        const returnOriginalOnFailure = 'true' === getValue('returnOriginalOnFailureSelector');
+        const fixImageOrientation = 'true' === getValue('fixImageOrientation');
         const compressedFile = await compress(file, {
             fixImageOrientation,
             maxHeight,
@@ -35,21 +31,24 @@ compressAndReportResults = async () => {
             returnOriginalOnFailure,
             scaleImageBy
         });
-        const compressedOrientation = await determineOrientation(compressedFile);
-        setCompressedDetails(compressedFile, compressedOrientation);
+        setCompressedDetails(compressedFile);
         setFileObjectUrl(compressedFile);
     }
 };
 
-const setOriginalDetails = (file, originalOrientation) => {
+const setOriginalDetails = async (file) => {
+    console.info('originalFile:', file)
     document.getElementById('originalSize').innerHTML = (file.size / 1024).toFixed(2);
     document.getElementById('originalType').innerHTML = file.type;
+    const originalOrientation = await determineOrientation(file);
     document.getElementById('originalOrientation').innerHTML = originalOrientation;
 };
 
-const setCompressedDetails = (file, compressedOrientation) => {
+const setCompressedDetails = async (file) => {
+    console.info('compressedFile:', file)
     document.getElementById('compressedSize').innerHTML = (file.size / 1024).toFixed(2);
     document.getElementById('compressedType').innerHTML = file.type;
+    const compressedOrientation = await determineOrientation(file);
     document.getElementById('compressedOrientation').innerHTML = compressedOrientation;
 };
 
